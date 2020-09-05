cbfc7d8cf44e664bf03ef61aea35674eb8570916
Pasi Huuhka <aakkoset@gmail.com>
2020-09-05 20:16:20 +0000

    Launch + HTTPS removal

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace codespaces
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
