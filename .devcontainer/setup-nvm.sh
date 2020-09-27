5711e6c271b3679ee328140a045db3f95445c5ce
Pasi Huuhka <aakkoset@gmail.com>
2020-09-27 17:07:03 +0000

    nvm?

---
+++ .devcontainer/setup-nvm.sh
@@ -0,0 +1,10 @@
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+
+source $NVM_DIR/nvm.sh
+nvm alias default ${NODE_VERSION}"
+
+if [ "$INSTALL_TYPESCRIPT" = "true" ]
+then
+  npm install -g typescript
+fi
